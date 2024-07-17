e012de183fdf1a913b0d32ba5519798fb3758dbe
Charles <charlespierse@gmail.com>
2024-07-17 10:20:46 +0100

    Add testing to ensure base imports work correctly (#6)

--- weaviate_recommend/services/recommendations/base.py
+++ weaviate_recommend/services/recommendations/base.py
@@ -1,9 +1,13 @@
-from weaviate_recommend.services import _ItemRecommendation
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from weaviate_recommend import WeaviateRecommendClient
+    from weaviate_recommend.services.recommendations.item import _ItemRecommendation
 
 
 class _Recommendation:
-    def __init__(self, client):
-        super().__init__(client)
+    def __init__(self, client: "WeaviateRecommendClient"):
+        self.client = client
         self.endpoint_url = f"{self.client.base_url}/recommendation/"
         self.item = _ItemRecommendation(client)
         self.user = None
